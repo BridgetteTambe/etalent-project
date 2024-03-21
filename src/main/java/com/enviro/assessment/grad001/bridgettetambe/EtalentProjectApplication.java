273159da1d3de4d532797d57bd580dfcac0c3c18
BridgetteTambe <bridgettetambe@gmail.com>
2024-03-21 13:58:42 +0200

    commit to git

---
+++ src/main/java/com/enviro/assessment/grad001/bridgettetambe/EtalentProjectApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.bridgettetambe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtalentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtalentProjectApplication.class, args);
+	}
+
+}
