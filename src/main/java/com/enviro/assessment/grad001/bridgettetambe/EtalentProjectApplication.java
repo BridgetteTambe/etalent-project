f3c421497db9a42a21b4fb749834edb310ecd7c8
BridgetteTambe <bridgettetambe@gmail.com>
2024-03-24 16:00:08 +0200

    working in progress.

--- src/main/java/com/enviro/assessment/grad001/bridgettetambe/EtalentProjectApplication.java
+++ src/main/java/com/enviro/assessment/grad001/bridgettetambe/EtalentProjectApplication.java
@@ -1,8 +1,13 @@
 package com.enviro.assessment.grad001.bridgettetambe;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@OpenAPIDefinition(info = @Info(title = " Etalent assessment API"))
+@EnableScheduling
 @SpringBootApplication
 public class EtalentProjectApplication {
 
--- src/main/java/com/enviro/assessment/grad001/bridgettetambe/repositories/EnvironmentalDataImportFileRepository.java
+++ src/main/java/com/enviro/assessment/grad001/bridgettetambe/repositories/EnvironmentalDataImportFileRepository.java
@@ -1,4 +1,17 @@
 package com.enviro.assessment.grad001.bridgettetambe.repositories;
 
-public class EnvironmentalDataImportFileRepository {
+
+import com.enviro.assessment.grad001.bridgettetambe.models.EnvironmentalDataImportFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EnvironmentalDataImportFileRepository extends JpaRepository<EnvironmentalDataImportFile, Long> {
+
+
+    List<EnvironmentalDataImportFile> findAllByProcessedFalse();
+    long  countEnvironmentalDataImportFileByFileHash(String fileHash);
 }
+
